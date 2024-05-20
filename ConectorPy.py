be610cc64309444618984d01aaf31a87a69782f4
Henrique Nalin <167473102+HenriqueNalin@users.noreply.github.com>
2024-05-20 19:03:10 -0300

    ConectorPy.py ( Funcionando! )

--- ConectorPy.py
+++ ConectorPy.py
@@ -1,13 +1,14 @@
-from mysql.connector import connect
+import mysql.connector
 
-def mysql_connection(host, user, passwd, database=None):
-    connection = connect(
+def mysql_connection(host, user, passwd, port, database=None):
+    connection = mysql.connector.connect(
         host = host,
         user = user,
         passwd = passwd,
+	      port = port,
         database = database
     )
     return connection
 
 connection = mysql_connection('pi-2024-omateocortez.c.aivencloud.com','avnadmin',
-                            'AVNS_mSrmiLQWmgRL7sxRVJ2', 'defaultdb')
+                            'AVNS_mSrmiLQWmgRL7sxRVJ2', '22705', 'defaultdb')
