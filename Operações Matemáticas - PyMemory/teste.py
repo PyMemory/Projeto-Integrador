b84bbf09a08475f40a9f161ccf50ec84195eddf7
Nicolas Pessoa <167625620+NicolasDeogenes@users.noreply.github.com>
2024-05-20 12:03:04 -0300

    Operações Matemáticas
    Temos um arquivo com uma classe com 4 diferentes funções que representam as contas que teremos no jogo, junto de um arquivo para teste.

---
+++ Operações Matemáticas - PyMemory/teste.py
@@ -0,0 +1,16 @@
+from Operacoes import Operacoes
+
+op = Operacoes()
+
+#teste adição
+x,y,resposta = op.adicao()
+print (x, "+", y, "=", resposta)
+#teste subtração
+x,y,resposta = op.subtracao()
+print (x, "-", y, "=", resposta)
+#teste multiplicação
+x,y,resposta = op.multiplicacao()
+print (x, "*", y, "=", resposta)
+#teste divisão
+x,y,resposta = op.divisao()
+print (x, "/", y, "=", resposta)
