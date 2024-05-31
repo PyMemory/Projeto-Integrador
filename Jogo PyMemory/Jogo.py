278451f962a29a2ff493f134fd8a1497c204f68b
Nicolas Pessoa <167625620+NicolasDeogenes@users.noreply.github.com>
2024-05-31 20:19:42 -0300

    Update Jogo.py

--- Jogo PyMemory/Jogo.py
+++ Jogo PyMemory/Jogo.py
@@ -1,32 +1,48 @@
 # Jogo.py
-from Fase0Teste import FaseT
-from Fase1 import Fase1
-from Fase2 import Fase2
-from Fase3 import Fase3
-from Fase4 import Fase4
+
+import Fase0Teste
+import Fase1
+import Fase2
+import Fase3
+import Fase4
 
 class Jogo:
-    def __init__(self):
-        self.fases = [
-            FaseT(),
-            Fase1(),
-            Fase2(),
-            Fase3(),
-            Fase4()
-        ]
+    @staticmethod
+    def calcular_pontuacao_final(pontos_fase1, pontos_fase2, pontos_fase3, pontos_fase4):
+        pontuacao_final = pontos_fase1 + pontos_fase2 + pontos_fase3 + pontos_fase4
+        if 30 <= pontuacao_final <= 40:
+            classificacao = "Incrível"
+        elif 20 <= pontuacao_final <= 29:
+            classificacao = "Ótima"
+        else:
+            classificacao = "Boa"
+        return pontuacao_final, classificacao
 
-    def Pontuacao_Final(self):
-        pontuacaoF = 0
-        pontuacaoF = sum(Fase1.pontos_fase1(), Fase2.pontos_fase2(),
-                         Fase3.pontos_fase3(), Fase4.pontos_fase4())
-        texto = str
-        if pontuacaoF > 40:
-            texto = "Incrível!"
-        elif pontuacaoF > 25:
-            texto = "Ótimo!"
-        elif pontuacaoF > 15:
-            texto = "Boa!"
-            
-        return pontuacaoF, texto
+    @staticmethod
+    def jogo():
+        # Executando fase 0
+        fase0 = Fase0Teste.FaseT()
+        fase0.executar_fase()
 
+        # Executando fase 1
+        fase1 = Fase1.Fase1()
+        fase1.executar_fase()
+        pontos_fase1 = fase1.pontos_fase1()
 
+        # Executando fase 2
+        fase2 = Fase2.Fase2()
+        fase2.executar_fase()
+        pontos_fase2 = fase2.pontos_fase2()
+
+        # Executando fase 3
+        fase3 = Fase3.Fase3()
+        fase3.executar_fase()
+        pontos_fase3 = fase3.pontos_fase3()
+
+        # Executando fase 4
+        fase4 = Fase4.Fase4()
+        fase4.executar_fase()
+        pontos_fase4 = fase4.pontos_fase4()
+
+        # Calcular pontuação final
+        pontuacao_final, classificacao = Jogo.calcular_pontuacao_final(pontos_fase1, pontos_fase2, pontos_fase3, pontos_fase4)
